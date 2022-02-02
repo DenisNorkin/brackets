d6670b4648c74a6a055d1a6245360c9742ab83a2
Denis.Norkin <denis.norkin.2019@gmail.com>
2022-02-02 09:52:57 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let bracketsStr = '';
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    bracketsStr += bracketsConfig[i][0] + bracketsConfig[i][1];
+  }
+  let cache = [];
+  for (let key of str) {
+    if (bracketsStr.indexOf(key) % 2 === 0) {
+      if (
+        key === bracketsStr[bracketsStr.indexOf(key) + 1] &&
+        cache[cache.length - 1] === bracketsStr.indexOf(key)
+      ) {
+        cache.pop();
+      } else {
+        cache.push(bracketsStr.indexOf(key));
+      }
+    } else {
+      if (cache.pop() !== bracketsStr.indexOf(key) - 1) {
+        return false;
+      }
+    }
+  }
+  return cache.length === 0;
+};
